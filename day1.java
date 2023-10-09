b0eac3686ff0d02496fbf6e96c48ce84be6e0540
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-09 13:39:38 +0800

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,18 @@
+package idamusdalifah;
+
+public class day1 {
+
+    public static void main(String[] args) {
+        String warnaKesukaan = "hitam";
+        String hobbySaya = "membaca";
+        String makananFavorit = "ayam";
+        int Umur = 20;
+
+        System.out.println("warna:" + warnaKesukaan);
+        System.out.println("hobby:" + hobbySaya);
+        System.out.println("makanan:" + makananFavorit);
+        System.out.println("umur:" + Umur);
+
+    }
+
+}
