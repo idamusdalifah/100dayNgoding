f00821560ae1641e9985aeefc5e3206a561d8376
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-10 20:29:41 +0800

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,15 @@
+package idamusdalifah;
+
+public class day2 {
+
+    public static void main(String[] args) {
+        String nama = "dipa";
+        int tinggiBadan = 160;
+        int beratBadan = 45;
+
+        System.out.println("nama:" + nama);
+        System.out.println("tinggi:" + tinggiBadan);
+        System.out.println("berat:" + beratBadan);
+
+    }
+}
