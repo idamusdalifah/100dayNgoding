417130c16d48b2a6ebef465f4d41c2dfe6db92b9
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-11 17:58:31 +0800

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,22 @@
+
+package idamusdalifah;
+
+
+public class day3 {
+     public static void main(String[] args) {
+        String nama = "ida musdalifah";
+        int umur = 20;
+        double tinggi = 156;
+        double berat = 47;
+        String jenis_kelamin = "perempuan";
+        char ukuran_baju = 'M';
+        
+        System.out.println("Nama Saya Adalah "+nama);
+        System.out.println("Umur Saya Sekarang Adalah "+umur+" Tahun");
+        System.out.println("Saya Memiliki Tinggi "+tinggi+" cm");
+        System.out.println("Saya Memiliki Berat "+berat+" kg");
+        System.out.println("Ukuran Baju Saya Adalah "+ukuran_baju);
+        System.out.println("Jenis Kelamin: "+jenis_kelamin);
+        
+     }   
+}
