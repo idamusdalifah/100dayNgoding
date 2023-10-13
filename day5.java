2ee74aa96335529a9f41c9e00274d3e3a95309ef
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-13 18:04:54 +0800

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,13 @@
+package idamusdalifah;
+
+public class day5 {
+
+    public static void main(String[] args) {
+        String nama = "balok";
+        int panjang = 8;
+        int lebar = 6;
+        int tinggi = 4;
+
+        System.out.println("volume = " + panjang * lebar * tinggi);
+    }
+}
