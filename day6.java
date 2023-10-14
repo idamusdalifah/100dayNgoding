5a7a8ad706b4b47a01ad1dbebdd5e2c27d11a579
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-14 18:30:26 +0800

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,21 @@
+package idamusdalifah;
+
+public class day6 {
+
+    public static void main(String[] args) {
+        String namaHewan = "ayam";
+        String warnaHewan = "putih";
+        String jenisHewan = "betina";
+        boolean ekor = true;
+        String bunyi = "cukurukuukk";
+        String makan = "jagung";
+
+        System.out.println("nama:" + namaHewan);
+        System.out.println("warna:" + warnaHewan);
+        System.out.println("jenis:" + jenisHewan);
+        System.out.println("ekor:" + ekor);
+        System.out.println("bunyi:" + bunyi);
+        System.out.println("makanan:" + makan);
+    }
+
+}
