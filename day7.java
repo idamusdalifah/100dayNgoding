54aac32333991e60b79fc6e54eab2d69cad03974
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-15 20:02:40 +0800

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,11 @@
+package idamusdalifah;
+
+public class day7 {
+
+    public static void main(String[] args) {
+        String keseharian = "saya selesai mengerjakan pekerjaan rumah";
+
+        System.out.println("hari ini:" + keseharian);
+    }
+
+}
