023d4783da336435af2ac786387f73a51e911111
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-16 21:40:12 +0800

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,21 @@
+package idamusdalifah;
+
+public class day8 {
+
+    public static void main(String[] args) {
+        String namaHewan = "kucing";
+        String warnaHewan = "putih";
+        String jenisHewan = "jantan";
+        boolean ekor = true;
+        String bunyi = "meonggg";
+        String makan = "ikan";
+
+        System.out.println("nama:" + namaHewan);
+        System.out.println("warna:" + warnaHewan);
+        System.out.println("jenis:" + jenisHewan);
+        System.out.println("ekor:" + ekor);
+        System.out.println("bunyi:" + bunyi);
+        System.out.println("makanan:" + makan);
+    }
+
+}
