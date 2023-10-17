2426b56a4f0ec419ac1b04a922e82b035be39eca
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-17 20:48:13 +0800

    Add files via upload

---
+++ day9.java
@@ -0,0 +1,11 @@
+package idamusdalifah;
+
+public class day9 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        System.out.println(numbers[1]);
+    }
+}
