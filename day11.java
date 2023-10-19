7428f97b0b76653f3cf0943e708c96d1eccbe35d
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-19 20:40:45 +0800

    Add files via upload

---
+++ day11.java
@@ -0,0 +1,13 @@
+package idamusdalifah;
+
+public class day11 {
+
+    public static void main(String[] args) {
+        String namaBuah = "apel,mangga,nanas";
+        String namaSayuran = "bayam,daun singkong,kol";
+
+        System.out.println("nama:" + namaBuah);
+        System.out.println("nama:" + namaSayuran);
+
+    }
+}
