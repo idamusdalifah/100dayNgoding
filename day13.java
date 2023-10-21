54d9a096ae398f8e1588b8f0cdaf78c0a522637c
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-21 18:03:43 +0800

    Add files via upload

---
+++ day13.java
@@ -0,0 +1,25 @@
+package idamusdalifah;
+
+public class day13 {
+
+    public static void main(String[] args) {
+        int[] angkaAngka = {1, 2};
+        int total = hitungTotal(angkaAngka);
+
+        System.out.print("Angka yang akan dijumlahkan: ");
+        for (int i = 0; i < angkaAngka.length; i++) {
+            System.out.print(angkaAngka[i] + (i < angkaAngka.length + 1 ? ", " : ""));
+        }
+
+        System.out.println("\nHasil penjumlahan: " + total);
+    }
+
+    private static int hitungTotal(int[] angkaAngka) {
+        int total = 0;
+        for (int angka : angkaAngka) {
+            total += angka;
+        }
+        return total;
+    }
+
+}
