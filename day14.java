df93a4be7e4260796c1022ef57d08d71514698cb
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-22 18:02:08 +0800

    Add files via upload

---
+++ day14.java
@@ -0,0 +1,11 @@
+package idamusdalifah;
+
+public class day14 {
+
+    public static void main(String[] args) {
+
+        boolean kondisi = true;
+        boolean hasil = !kondisi;
+        System.out.println("NOT kondisi: " + hasil);
+    }
+}
