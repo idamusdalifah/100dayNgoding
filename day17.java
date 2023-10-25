791ab60503c9b369695c710ec546899e193648c2
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-25 20:09:08 +0800

    Add files via upload

---
+++ day17.java
@@ -0,0 +1,11 @@
+package idamusdalifah;
+
+public class day17 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
