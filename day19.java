bc35e1525cfb5f72b47c231e078cc4a73e6bfcfa
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-27 19:21:05 +0800

    Add files via upload

---
+++ day19.java
@@ -0,0 +1,12 @@
+package idamusdalifah;
+
+public class day19 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 99; i++) {
+            System.out.println("INFORMATIKA");
+
+        }
+    }
+
+}
