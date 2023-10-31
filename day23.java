2a862b24dfa1d0426ad0ee46300456415a447ca3
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-10-31 17:44:11 +0800

    Add files via upload

---
+++ day23.java
@@ -0,0 +1,14 @@
+package idamusdalifah;
+
+public class day23 {
+
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 3;
+        int hasil = angka1 * angka2;
+
+        System.out.println("Hasil perkalian dari " + angka1 + " dan " + angka2 + " adalah: " + hasil);
+    }
+}
+
+        
