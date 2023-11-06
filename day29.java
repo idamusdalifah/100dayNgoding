c04e98084bc96c9ef660bb7f2dd75d4f61003dc1
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-11-06 19:27:49 +0800

    Add files via upload

---
+++ day29.java
@@ -0,0 +1,11 @@
+package idamusdalifah;
+
+public class day29 {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+        System.out.println("Hasil penambahan: " + sum);
+    }
+}
