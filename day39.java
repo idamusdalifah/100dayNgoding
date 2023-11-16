927fcd5eff45b8b236d2042cb1ff0b1b76f9301d
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-11-16 20:12:32 +0800

    Add files via upload

---
+++ day39.java
@@ -0,0 +1,17 @@
+package idamusdalifah;
+
+public class day39 {
+
+    public static void main(String[] args) {
+
+        int panjang = 6;
+        int lebar = 8;
+
+        int luas = panjang * lebar;
+
+        int keliling = 2 * (panjang + lebar);
+
+        System.out.println("Luas Persegi Panjang: " + luas);
+        System.out.println("Keliling Persegi Panjang: " + keliling);
+    }
+}
