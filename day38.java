97dfb9d56f712df06d3df1d8a33d181f77f0114d
idamusdalifah <100522645+idamusdalifah@users.noreply.github.com>
2023-11-16 20:12:14 +0800

    Add files via upload

---
+++ day38.java
@@ -0,0 +1,13 @@
+package idamusdalifah;
+
+public class day38 {
+
+    public static void main(String[] args) {
+
+        int angka1 = 10;
+        int angka2 = 20;
+        int hasil = angka1 + angka2;
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+
+}
